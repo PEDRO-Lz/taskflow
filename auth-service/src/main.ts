82b3b46e975a0a4e5ed482d541bed302d42c8202
pedro.souza <pedro.souza@irriga.net>
2026-09-02 22:25:23 -0300

    feat: add auth initial routes

---
+++ auth-service/src/main.ts
@@ -0,0 +1,17 @@
+import 'dotenv/config'
+import { NestFactory } from '@nestjs/core'
+import { ValidationPipe } from '@nestjs/common'
+import { MikroORM } from '@mikro-orm/core'
+import { AppModule } from './app.module'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }))
+
+  // migrations
+  const orm = app.get(MikroORM)
+  await orm.schema.update()
+
+  await app.listen(3001)
+}
+bootstrap()
